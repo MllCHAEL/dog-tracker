c7e1923083b1549c2b570373fc6db3287c97e4eb
Michael Havenga <Michael.A.Havenga@gmail.com>
2023-01-20 23:18:57 +0800

    - Created reactive form - Added validators to form controls - 'Add dog' button now disabled if form is invalid

--- dog-tracker-front-end/src/app/dog-form/dog-form.component.ts
+++ dog-tracker-front-end/src/app/dog-form/dog-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dog-form',
@@ -6,6 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./dog-form.component.scss']
 })
 
-export class DogFormComponent {
+export class DogFormComponent implements OnInit {
 
+  // TODO: Add validation for pure whitespace input (e.g. just spacebars)
+  dogForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+    barksALot: new FormControl(false, Validators.required)
+  });
+  // TODO: Display validation error messages (E.g. "Char limit (20) exceeded")
+
+  addDog(): void {
+    if (this.dogForm.valid) {
+      console.log('Dog name:' + this.dogForm.controls.name.value, 'Barks a lot?', this.dogForm.controls.barksALot.value);
+      this.dogForm.reset();
+      this.dogForm.controls.barksALot.setValue(false);
+      return
+    }
+    this.dogForm.reset();
+    this.dogForm.controls.barksALot.setValue(false);
+    console.log('Invalid dog submission. Fields have been reset. ')
+  }
+
+  ngOnInit() {
+    // TODO: ngOnInit if it is never used (think it will be used though :p)
+  }
 }
