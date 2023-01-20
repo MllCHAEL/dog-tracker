258da03457d519ddec4d29735e54280d92519023
Michael Havenga <Michael.A.Havenga@gmail.com>
2023-01-20 20:15:07 +0800

    - Added html table using placeholder dog data from TS - Created Dog class (dog.ts) - Imported ReactiveFormsModule in app.module (expecting use soon)

--- dog-tracker-front-end/src/app/app.component.ts
+++ dog-tracker-front-end/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Dog } from './dog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dog-tracker-front-end';
+
+
+export class AppComponent implements OnInit {
+
+  dogs: Dog[] = [];
+
+  ngOnInit() {
+    this.dogs = [{ id: 1, name: 'Bear', barksALot: true },
+    { id: 2, name: 'Jazzie', barksALot: false },
+    { id: 5, name: 'Barkley', barksALot: true },
+    { id: 3, name: 'Roofas', barksALot: true },
+    { id: 4, name: 'Woofred', barksALot: false }];
+  }
 }
--- dog-tracker-front-end/src/app/app.module.ts
+++ dog-tracker-front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
