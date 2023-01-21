05d36313d80b5ede99abd8fa8f39df1938320ea0
Michael Havenga <Michael.A.Havenga@gmail.com>
2023-01-21 22:21:52 +0800

    - Added pipe(take(1)) on GET dogs list to prevent memory leaks

--- dog-tracker-front-end/src/app/dog-list/dog-list.component.ts
+++ dog-tracker-front-end/src/app/dog-list/dog-list.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
 import { Dog } from '../dog';
 
 @Component({
@@ -14,6 +15,7 @@ export class DogListComponent implements OnInit {
   constructor(private http: HttpClient) { };
 
   ngOnInit() {
-    this.http.get<Dog[]>('http://localhost:7071/api/GetDogs').subscribe(dogs => this.dogList = dogs)
+    this.http.get<Dog[]>('http://localhost:7071/api/GetDogs').pipe(take(1)).subscribe(
+      dogs => this.dogList = dogs)
   };
 }
