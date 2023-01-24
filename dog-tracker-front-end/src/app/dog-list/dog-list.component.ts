6d5fbf554a45c4e8f34d900e3ea5846ac5d65b8f
Michael Havenga <Michael.A.Havenga@gmail.com>
2023-01-25 02:34:27 +0800

    When a dog is being deleted a loading spinner is now shown instead of a delete (trash) icon.

--- dog-tracker-front-end/src/app/dog-list/dog-list.component.ts
+++ dog-tracker-front-end/src/app/dog-list/dog-list.component.ts
@@ -12,13 +12,17 @@ import { DogService } from '../dog.service';
 
 export class DogListComponent {
 
+  public dogsToDelete: string[] = [];
+
   constructor(private http: HttpClient, public dogService: DogService) { };
 
   deleteDog(dog: Dog) {
     if (confirm(`Are you sure to delete this dog?: ${dog.name}`)) {
+      var dogIndex = this.dogsToDelete.push(dog.id) - 1;
       // TODO: Encode dogData before appending to url (if mimicking sensitive data)
       // TODO: Add deleting indicator (e.g. swap delete icon to mat spinner)
-      concat(this.dogService.deleteDog(dog), this.dogService.getDogList()).subscribe();
+      concat(this.dogService.deleteDog(dog), this.dogService.getDogList()).
+        subscribe(dogDeleted => this.dogsToDelete.splice(dogIndex, 1));
     }
   }
 
